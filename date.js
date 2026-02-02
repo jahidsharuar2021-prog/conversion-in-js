19695921709d0d49be5357d77058da0bc0f3320d
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-02-02 19:48:03 +0600

    time zone

---
+++ date.js
@@ -0,0 +1,13 @@
+
+
+Date function useing tricks
+
+const today=new Date();
+console.log(today);
+console.log('another example');
+const date =new Date('2020-11-2');  //secriet trips aii kane index dara kinthu hesab kora hoi
+console.log(date.getDay());
+const specificdate=new Date(2026, 0 ,26);
+console.log(specificdate);
+// specificdate.setMonth(10);
+console.log(specificdate.toLocaleString('en-bd'));
